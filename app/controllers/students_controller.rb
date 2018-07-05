f9d7ca159f9023ca64a53b30f20c06f69ce8ad79
Andrew Kairys <andrewkairys@gmail.com>
2018-07-05 17:11:12 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,16 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
+
+  end
+
 end
